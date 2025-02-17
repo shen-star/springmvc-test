a579ccff502f5542c75d2de23c60347f61ec9ae5
Alex <1172068437@qq.com>
2025-02-17 21:28:23 +0800

    build: 配置Thymeleaf视图解析器，添加index页面和返回视图的方法 v1.0

---
+++ springmvc-first-demo/src/main/java/com/shen/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.shen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        //返回视图名称(前缀和后缀已在web.xml中配置了)
+        return "index";
+    }
+}
