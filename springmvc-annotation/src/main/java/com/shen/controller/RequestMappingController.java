f28c71e0ba122d1f040a17b3141e1ab89711098a
Alex <1172068437@qq.com>
2025-03-10 22:13:57 +0800

    feat: @RequestMapping注解的位置，标识一个类与标识一个方法的区别 v1.0

--- springmvc-annotation/src/main/java/com/shen/controller/RequestMappingController.java
+++ springmvc-annotation/src/main/java/com/shen/controller/RequestMappingController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("hello")
 public class RequestMappingController {
-    @RequestMapping("/")
-    public String index() {
-        return "target";
+    @RequestMapping("/testRequestMapping")
+    public String success() {
+        return "success";
     }
 
 }
