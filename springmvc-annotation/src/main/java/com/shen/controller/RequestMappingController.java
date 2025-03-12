2bea1c7928c54c7a6d87dd73b86100adda5340d1
Alex <1172068437@qq.com>
2025-03-12 22:02:42 +0800

    feat: @RequestMapping的派生注解,@GetMapping指定http请求方式，使用get请求方式 v1.0

--- springmvc-annotation/src/main/java/com/shen/controller/RequestMappingController.java
+++ springmvc-annotation/src/main/java/com/shen/controller/RequestMappingController.java
@@ -1,6 +1,7 @@
 package com.shen.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,6 +21,18 @@ public class RequestMappingController {
     @RequestMapping(value = {"/testMethod"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String testMethod() {
         return "testmethod";
+    }
+
+    /**
+     * -@RequestMapping的派生注解
+     * 处理get请求的映射-->@GetMapping
+     * 处理post请求的映射-->@PostMapping
+     * 处理put请求的映射-->@PutMapping
+     * 处理delete请求的映射-->@DeleteMapping
+     */
+    @GetMapping(value = {"/testGetMapping"})
+    public String testGetMappingIndex() {
+        return success();
     }
 
 }
