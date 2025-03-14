ea645673c5de09f54482c4bd666d5f467c26bb43
Alex <1172068437@qq.com>
2025-03-14 23:53:40 +0800

    feat: springMVC获取请求参数-通过控制器方法的形参获取请求参数 v1.0

--- springmvc-annotation/src/main/java/com/shen/controller/ParamController.java
+++ springmvc-annotation/src/main/java/com/shen/controller/ParamController.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+
 @Controller
 @RequestMapping("/param")
 public class ParamController {
@@ -13,6 +15,23 @@ public class ParamController {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         System.out.println("username : " + username + ",password : " + password);
+        return "success";
+    }
+
+    @RequestMapping("/testParam")
+    public String testServletAPI(String username, String password) {
+        System.out.println("username : " + username + ",password : " + password);
+        return "success";
+    }
+
+    /**
+     * 若请求所传输的请求参数中有多个同名的请求参数，此时可以在控制器方法的形参中设置字符串数组或者字符串类型的形参接收此请求参数
+     * 若使用字符串数组类型的形参，此参数的数组中包含了每一个数据
+     * 若使用字符串类型的形参，此参数的值为每个数据中间使用逗号拼接的结果
+     */
+    @RequestMapping("/testParam2")
+    public String testServletAPI(String username, String password, String[] hobby) {
+        System.out.println("username : " + username + ",password : " + password + ",hobby : " + Arrays.toString(hobby));
         return "success";
     }
 }
