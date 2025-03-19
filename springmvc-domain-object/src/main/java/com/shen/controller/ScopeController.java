530752e330d521e1be31d6f63595208321fce15f
Alex <1172068437@qq.com>
2025-03-20 00:25:53 +0800

    feat: springMVC-向Request域对象共享数据-通过使用ModelAndView的方式 v1.0

--- springmvc-domain-object/src/main/java/com/shen/controller/ScopeController.java
+++ springmvc-domain-object/src/main/java/com/shen/controller/ScopeController.java
@@ -3,6 +3,7 @@ package com.shen.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -13,5 +14,17 @@ public class ScopeController {
     public String testRequestByServletAPI(HttpServletRequest httpServletRequest) {
         httpServletRequest.setAttribute("testRequestScope", "this is test scope by Servlet API");
         return "success";
+    }
+
+    @RequestMapping("/testModelAndView")
+    public ModelAndView testModelAndView() {
+        //ModelAndView有Model和View的功能
+        //Model主要用于向请求域共享数据
+        //View主要用于设置视图，页面跳转
+        ModelAndView mav = new ModelAndView();
+        //处理模型数据，即向请求域request共享数据
+        mav.addObject("testModelAndView", "this is test scope by ModelAndView");
+        mav.setViewName("success");
+        return mav;
     }
 }
